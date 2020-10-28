513749f5af7c4d1a4d617d66a4ab53789d050c81
ioiazay [remote] <my.ioiaid@gmail.com>
2020-10-28 08:35:13 +0700

    [DONE] Test Libraries

---
+++ uji/hallo.py
@@ -0,0 +1,23 @@
+import cirq
+import sympy
+import numpy as np
+import tensorflow as tf
+# import tensorflow_quantum
+
+# Pick a qubit.
+qubit = cirq.GridQubit(0, 0)
+
+# Create a circuit
+circuit = cirq.Circuit(
+    cirq.X(qubit)**0.5,  # Square root of NOT.
+    cirq.measure(qubit, key='m')  # Measurement.
+)
+
+print("Circuit:")
+print(circuit)
+
+# Simulate the circuit several times.
+simulator = cirq.Simulator()
+result = simulator.run(circuit, repetitions=20)
+print("Results:")
+print(result)
